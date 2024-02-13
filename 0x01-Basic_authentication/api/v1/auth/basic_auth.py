3a57096d051599a03a8765ef281ecc086e60cf8e
younis-alx <younialx-swe@hotmail.com>
2024-02-13 14:12:59 +0300

    Refactor extract_base64_authorization_header method signature

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -10,10 +10,14 @@ class BasicAuth(Auth):
     """ BasicAuth class
     """
 
-    def extract_base64_authorization_header(self, authorization_header: str) -> str:
+    def extract_base64_authorization_header(
+        self,
+        authorization_header: str
+    ) -> str:
         """ Extracts the base64 part of the authorization header
         """
-        if authorization_header is None or not isinstance(authorization_header, str):
+        if authorization_header is None or\
+                not isinstance(authorization_header, str):
             return None
         if not authorization_header.startswith("Basic "):
             return None
