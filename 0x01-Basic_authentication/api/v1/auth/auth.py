f79800dc736903598f18710d9a427716f5fe18ea
younis-alx <younialx-swe@hotmail.com>
2024-02-13 13:47:13 +0300

    Refactor authentication check logic in Auth class***

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -24,7 +24,14 @@ class Auth:
         Returns:
             bool: True if authentication is required, False otherwise.
         """
-        return False
+        if path is None or not excluded_paths:
+            return True
+
+        for excluded_path in excluded_paths:
+            if path.startswith(excluded_path):
+                return False
+
+        return True
 
     def authorization_header(self, request=None) -> str:
         """
