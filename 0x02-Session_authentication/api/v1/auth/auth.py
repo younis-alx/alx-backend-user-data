3afe099366679d941fcdf616a5f925d16c53530e
younis-alx <younialx-swe@hotmail.com>
2024-02-15 10:29:19 +0300

    Add authentication module

---
+++ 0x02-Session_authentication/api/v1/auth/auth.py
@@ -0,0 +1,64 @@
+#!/usr/bin/env python3
+
+"""
+    This module is the template for all authentication systems.
+"""
+from typing import List, TypeVar
+from flask import request
+
+
+class Auth:
+    """
+    This class provides authentication functionality for the API.
+    """
+
+    def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
+        """
+        Checks if authentication is required for the given path.
+
+        Args:
+            path (str): The path to check.
+            excluded_paths (List[str]): List of paths that
+                are excluded from authentication.
+
+        Returns:
+            bool: True if authentication is required, False otherwise.
+        """
+        if path is None or not excluded_paths:
+            return True
+
+        path = path.rstrip('/')
+        for excluded_path in excluded_paths:
+            excluded_path = excluded_path.rstrip('/')
+            if path == excluded_path:
+                return False
+
+        return True
+
+    def authorization_header(self, request=None) -> str:
+        """
+        Retrieves the authorization header from the request.
+
+        Args:
+            request (flask.Request, optional): The request object.
+                Defaults to None.
+
+        Returns:
+            str: The authorization header value.
+        """
+        if request is None or 'Authorization' not in request.headers:
+            return None
+        return request.headers['Authorization']
+
+    def current_user(self, request=None) -> TypeVar('User'):
+        """
+        Retrieves the current user based on the request.
+
+        Args:
+            request (flask.Request, optional): The request object.
+                Defaults to None.
+
+        Returns:
+            User: The current user object.
+        """
+        return None
